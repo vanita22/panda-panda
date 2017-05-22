876e5c432b08586fc77c10220b390ad06c47edfb
vanita22 <vanitayrafita2004@gmail.com>
2017-05-22 01:50:21 -0300

    subiendo archivos

---
+++ assets/js/java.js
@@ -0,0 +1,18 @@
+function limpiar(){
+var a = document.getElementById("pand1");
+var b = document.getElementById("pand2");
+var c = document.getElementById("pand3");
+var d = document.getElementById("pand4");
+
+while (a.firstChild) {
+  a.removeChild(a.firstChild);
+
+while (b.firstChild) {
+  b.removeChild(b.firstChild);
+
+while (c.firstChild) {
+  c.removeChild(c.firstChild);
+
+while (d.firstChild) {
+  d.removeChild(d.firstChild);
+}
